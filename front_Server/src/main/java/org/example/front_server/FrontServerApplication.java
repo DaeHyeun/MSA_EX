94a84d17e7f2c69c09fe3c1a39f8d318c5256364
Dae Hyun <dh1236@naver.com>
2025-01-09 13:17:04 +0900

    layout

---
+++ front_Server/src/main/java/org/example/front_server/FrontServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.front_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontServerApplication.class, args);
+    }
+
+}
